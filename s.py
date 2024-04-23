1ec356691507ffed22a075d8b41b82e6ced763e0
Virschnieder <agrawalmahavir007@gmail.com>
2024-04-23 21:27:41 +0200

    new commit

--- s.py
+++ s.py
@@ -47,6 +47,9 @@ def compact_size_integer_to_int(value):
         return 4
     else:
         return 8
+#convert 8bytes little endian to decimal
+def little_endian_to_int(bytes_data):
+    return int.from_bytes(bytes_data, byteorder='little')
     
 # Path to the folder containing JSON files
 folder_path = "./mempool"
@@ -97,31 +100,31 @@ with open('output.txt', 'w') as y:
                 value = [vout.get("value") for vout in data.get("vout", [])]
                 lockingscript = [vout.get("scriptpubkey") for vout in data.get("vout", [])]
                 scriptpubkey = [vin.get("prevout", {}).get("scriptpubkey") for vin in data.get("vin", [])]
-                value_out = [vout.get("value") for vout in data.get("vout", [])]
-                value_in = value
+                v_in = [vin.get("prevout", {}).get("value") for vin in data.get("vin", [])]
+
                 # Print the txids and vouts
                 n_txids = len(txids)
                 output_count = len(value)
                 
                 # Convert the version number to little endian and then to hex
                 version_hex = int_to_hex(version)
-                print(f"Version number in little endian: {version_hex}")
+                #print(f"Version number in little endian: {version_hex}")
                 #append the version to the data_c
                 data_c += version_hex
                 array_s_m.append(version_hex)
                 Input_count = int_to_hex_compact_size_integer(n_txids)
                 data_c += Input_count
                 array_s_m.append(Input_count)
-                print(f"Input Count: {Input_count}")
+                #print(f"Input Count: {Input_count}")
                 for i in range(n_txids):
                     #convert the txid to reverse byte
                     txids[i] = reverse_byte(bytes.fromhex(txids[i]))
-                    print(f"Txid: {txids[i]}")
+                    #print(f"Txid: {txids[i]}")
                     data_c += txids[i]
                     array_s_m.append(txids[i])
                     #convert vout to little endian and then to hex
                     vouts[i] = int_to_hex(vouts[i])
-                    print(f"Vout: {vouts[i]}")
+                    #print(f"Vout: {vouts[i]}")
                     data_c += vouts[i]
                     array_s_m.append(vouts[i])
                     
@@ -130,39 +133,39 @@ with open('output.txt', 'w') as y:
                     #scriptsig_size in bytes is half of scriptsig_size in hex, don't make scriptsig_size float
                     scriptsig_size = scriptsig_size//2
                     scriptsig_size = int_to_hex_compact_size_integer(scriptsig_size)
-                    print(f"ScriptSig Size: {scriptsig_size}")
+                    #print(f"ScriptSig Size: {scriptsig_size}")
                     data_c += scriptsig_size
                     array_s_m.append("00")
-                    print(f"ScriptSig: {scriptsig[i]}")
+                    #print(f"ScriptSig: {scriptsig[i]}")
                     data_c += scriptsig[i]
                     sequence[i] = int_to_hex(sequence[i])
-                    print(f"Sequence: {sequence[i]}") 
+                    #print(f"Sequence: {sequence[i]}") 
                     data_c += sequence[i]
                     array_s_m.append(sequence[i])
                 
                 Output_count = int_to_hex_compact_size_integer(output_count)
-                print (f"Output Count: {Output_count}")
+                #print (f"Output Count: {Output_count}")
                 data_c += Output_count
                 array_s_m.append(Output_count)
                 for i in range(output_count):
                     value[i] = int_to_little_endian_8bytes(int(value[i]))
-                    print(f"Amount: {value[i]}")
+                    #print(f"Amount: {value[i]}")
                     data_c += value[i]
                     array_s_m.append(value[i])
                     scriptpubkey_size = len(lockingscript[i])
                     scriptpubkey_size = scriptpubkey_size//2
                     scriptpubkey_size = int_to_hex_compact_size_integer(scriptpubkey_size)
-                    print(f"Locking Script Size: {scriptpubkey_size}")
+                    #print(f"Locking Script Size: {scriptpubkey_size}")
                     data_c += scriptpubkey_size
                     array_s_m.append(scriptpubkey_size)
-                    print(f"Locking Script: {lockingscript[i]}")
+                    #print(f"Locking Script: {lockingscript[i]}")
                     data_c += lockingscript[i]
                     array_s_m.append(lockingscript[i])
                 locktime = int_to_hex(locktime)
-                print(f"Locktime: {locktime}")
+                #print(f"Locktime: {locktime}")
                 data_c += locktime
                 array_s_m.append(locktime)
-                print(f"Data_c: {data_c}")
+                #print(f"Data_c: {data_c}")
                 #print array_s_m
 
                 array_message = []
@@ -181,12 +184,12 @@ with open('output.txt', 'w') as y:
                     message = ""
                     #message is the concatenation of all the elements in the array
                     for i in temp_a:
-                        print(f"i: {i}")
+                        #print(f"i: {i}")
                         message += i
                     #double sha256 the message
                     by_string = bytes.fromhex(message)
                     hash_message = hashlib.sha256(hashlib.sha256(by_string).digest()).digest()
-                    print(f"Message: {hash_message.hex()}")
+                    #print(f"Message: {hash_message.hex()}")
                     array_message.append(hash_message.hex())
                 array_r = []
                 array_s = []
@@ -198,75 +201,107 @@ with open('output.txt', 'w') as y:
                     length_signature = int(scriptsig[i][:2], 16)
                     #next length_signature*2 bytes is the signature
                     signature = scriptsig[i][2:2+length_signature*2]
-                    print(f"Signature: {signature}")
+                    #print(f"Signature: {signature}")
                     #discard the first 6 hexadecimal characters, the next 2 hexadecimal characters is the length of r
                     length_r = int(signature[6:8], 16)
                     #next length_r*2 bytes is r
                     r = signature[8:8+length_r*2]
-                    print(f"r: {r}")
+                    #print(f"r: {r}")
                     #discard the next 2 hexadecimal characters, the next 2 hexadecimal characters is the length of s
                     length_s = int(signature[10+length_r*2:12+length_r*2], 16)
                     #next length_s*2 bytes is s discarding the last 2 hexadecimal characters
                     s = signature[12+length_r*2:12+length_r*2+length_s*2]
-                    print(f"s: {s}")
+                    #print(f"s: {s}")
                     #next 2 bytes is the length of the public key 
                     length_public_key = int(scriptsig[i][2+length_signature*2:2+length_signature*2+2], 16)
-                    print(f"Length of Public Key: {length_public_key}")
+                    #print(f"Length of Public Key: {length_public_key}")
                     #next length_public_key*2 bytes is the public key
                     public_key = scriptsig[i][2+length_signature*2+2:]
-                    print(f"Public Key: {public_key}")
+                    #print(f"Public Key: {public_key}")
                     array_r.append(r)
                     array_s.append(s)
                     array_public_key.append(public_key)
                 #print array_public_key, array_r, array_s, array_message
-                print(f"Array Public Key: {array_public_key}")
+                """ print(f"Array Public Key: {array_public_key}")
                 print(f"Array r: {array_r}")
                 print(f"Array s: {array_s}")
                 print(f"Array Message: {array_message}")
-
+ """
                 for i in range(n_txids):
                     pub_key = array_public_key[i]
                     sig = {'r': int(array_r[i],16), 's': int(array_s[i],16)}
                     mgs = int(array_message[i],16)
                     result = v.verify_signature(pub_key, sig, mgs)
-                    if result == True:
-                        print("True")
-                    else:
-                        print("False")
+                    if result != True:
                         flag = 1
-                
-                
-                
+
                 if flag == 1:
                     count_valid_tx += 1
                 bytes_string = bytes.fromhex(data_c)
                 hash = hashlib.sha256(hashlib.sha256(bytes_string).digest()).digest()
 
-                hash = reverse_byte(hash)
-                #put this hash into txid set if flag = 0
                 if flag == 0:
-                    txid_set.add(hash)
+                    txid_set.add(hash.hex())
+                
+                hash = reverse_byte(hash)
                 hash = hashlib.sha256(bytes.fromhex(hash)).digest()
-                print(f"Hash: {hash.hex()}")
-                print(f"File: {filename}")
-                print("\n")
+                #print(f"Hash: {hash.hex()}")
+                #print(f"File: {filename}")
+                #print(f"value: {value}")
+                #iterate over the value and convert it into integer using little endian to integer and add it to fee
+                temp_out = 0
+                for i in value:
+                    temp_out += little_endian_to_int(bytes.fromhex(i))
+                temp_in = 0
+                for i in v_in:
+                    temp_in += i
+                fee += temp_in - temp_out
+                #print("\n")
                 #check if hash + .json is equal to the file name or not
                 if hash.hex() + ".json" == filename:
                     temp += 1
                 
+
+
     coutt = counter - count_valid_tx
     #print the txid_set and it's size
-    print(f"Txid Set: {txid_set}")
-    print(f"Size of Txid Set: {len(txid_set)}")
-    print(f"Total number of files with all valid scriptpubkey_type: {counter}")
-    print(f"Total number of files with valid hash: {temp}")
-    print(f"Number of valid transactions: {coutt}")
-    print(f"Total fee: {fee}")
+    #print(f"Txid Set: {txid_set}")
+    #print(f"Size of Txid Set: {len(txid_set)}")
+    #print(f"Total number of files with all valid scriptpubkey_type: {counter}")
+    #print(f"Total number of files with valid hash: {temp}")
+    #print(f"Number of valid transactions: {coutt}")
+    #print(f"Total fee: {fee}")
+
+    #creating coinbase transaction
+    ver_c = "01000000"
+    input_cc = "01"
+    input_c ="0000000000000000000000000000000000000000000000000000000000000000"
+    vout_c = "ffffffff"
+    scriptsig_size_c = "1b"
+    scriptsig_c = "03951a0604f15ccf5609013803062b9b5a0100072f425443432f20"
+    sequence_c = "00000000"
+    output_cc = "01"
+    amount = fee + 625000000
+    amount = int_to_little_endian_8bytes(amount)
+    scriptpubkey_size_c = "19"
+    scriptpubkey_c = "76a9142c30a6aaac6d96687291475d7d52f4b469f665a688ac"
+    locktime_c = "00000000"
+    coinbase = ver_c + input_cc + input_c + vout_c + scriptsig_size_c + scriptsig_c + sequence_c + output_cc + amount + scriptpubkey_size_c + scriptpubkey_c + locktime_c
+    #print(f"Coinbase: {coinbase}")
+    #hash256 of the coinbase
+    coinbase = bytes.fromhex(coinbase)
+    coinbase_hash = hashlib.sha256(hashlib.sha256(coinbase).digest()).digest()
+    #print(f"Coinbase Hash: {coinbase_hash.hex()}")
+    
+
     #merkel root calculation using the txid_set
     merkel_root = []
 
     # Convert txid_set to a list
     txid_list = list(txid_set)
+    txid_list.insert(0,coinbase_hash.hex())
+    #make a copy of txid_list
+    final_txid_list = txid_list.copy()
 
     while len(txid_list) > 1:
         temp_t = []
@@ -276,12 +311,14 @@ with open('output.txt', 'w') as y:
                 concatenated_hash = hashlib.sha256(hashlib.sha256(bytes.fromhex(txid_list[i] + txid_list[i+1])).digest()).digest().hex()
                 temp_t.append(concatenated_hash)
             else:
-                temp_t.append(txid_list[i])
+                #if there is a single txid then concatenate it with itself and hash it
+                concatenated_hash = hashlib.sha256(hashlib.sha256(bytes.fromhex(txid_list[i] + txid_list[i])).digest()).digest().hex()
+                temp_t.append(concatenated_hash)
         txid_list = temp_t
 
     # The resulting merkel root will be the first element of txid_list
     merkel_root = txid_list[0]
-    print(f"Merkel Root: {merkel_root}")
+    #print(f"Merkel Root: {merkel_root}")
 
     #block header creation
     version_bh = "02000000"
@@ -290,12 +327,33 @@ with open('output.txt', 'w') as y:
     current_time = int_to_little_endian_4bytes(current_time)
     bits = "1f00ffff"
     nonce = "00000000"
+    #iterate over all nonce values till the block hash is less than the target
+    target = "0000ffff00000000000000000000000000000000000000000000000000000000"
+    target_int = int(target, 16)
     block_header = version_bh + previous_block_hash + merkel_root + current_time + bits + nonce
-    print(f"Block Header: {block_header}")
+    block_header = bytes.fromhex(block_header)
+    block_hash = hashlib.sha256(hashlib.sha256(block_header).digest()).digest()
+    block_hash_int = int(block_hash.hex(),16)
+
+    while block_hash_int > target_int and nonce != "ffffffff":
+        nonce = int_to_little_endian_4bytes(int(nonce, 16) + 1)
+        block_header = version_bh + previous_block_hash + merkel_root + current_time + bits + nonce
+        block_header = bytes.fromhex(block_header)
+        block_hash = hashlib.sha256(hashlib.sha256(block_header).digest()).digest()
+        block_hash_int = int(block_hash.hex(), 16)
+
+    block_header = version_bh + previous_block_hash + merkel_root + current_time + bits + nonce
+    print(f"{block_header}")
+    print(f"{coinbase.hex()}")
     #hash256 of the block header
     block_header = bytes.fromhex(block_header)
     block_hash = hashlib.sha256(hashlib.sha256(block_header).digest()).digest()
-    print(f"Block Hash: {block_hash.hex()}")
+    #print(f"Block Hash: {block_hash.hex()}")
+    #print(f"Nonce: {nonce}")
+    #print the final_txid_list in reverse byte order
+    for i in range(len(final_txid_list)):
+        final_txid_list[i] = reverse_byte(bytes.fromhex(final_txid_list[i]))
+        print(final_txid_list[i])
 
     sys.stdout = original_stdout
             
