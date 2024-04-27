8f871ef554fe448b442290fd8f733583081ab20a
Virschnieder <agrawalmahavir007@gmail.com>
2024-04-27 07:58:13 +0200

    Add changes

--- s.py
+++ s.py
@@ -94,7 +94,7 @@ with open('output.txt', 'w') as y:
                 if scriptpubkey_type not in ["v0_p2wpkh"]:
                     all_valid_segwit = False
                     break
-            
+            all_valid_segwitm = False
             if all_valid_segwit and counter_segwit < 1528:
                 counter_segwit += 1
                 flag = 0
